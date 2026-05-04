5a6f6e14e9768a005441ffe25b063ffe1d356866
mahmoudforprogramming-design <mahmoudforprogramming@gmail.com>
2026-05-04 22:39:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+setTimeout(function() {
+  document.getElementById("popupOverlay").style.display = "flex";
+}, 16000);
+
+function closePopup() {
+  document.getElementById("popupOverlay").style.display = "none";
+}
